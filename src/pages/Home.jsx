54dd12b3d82a5545bddd127a7f540805ce047c2c
tb0ied <pchonacki@proton.me>
2023-10-27 13:11:41 +0100

    Router Routes Route

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return;
+
+  <>
+    <div>
+      <main>
+        <h2>Home Page</h2>
+        <p>
+          React Router Dom allows us to have multiple pages in our SPA (Single
+          Page Application)
+        </p>
+      </main>
+    </div>
+  </>;
+}
